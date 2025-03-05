3bffafeaa7b8c70a3922e8abe78222a0bc85d42c
pirofestionalDev <r4roggit1@gmail.com>
2025-03-06 02:31:09 +0530

    changes in the header file

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from 'react'
+import { AlignJustify,Search } from 'lucide-react';
+import Link from 'next/link';
 
-export default function Header() {  
-  return (   
-    <div className="flex items-center justify-between p-4 bg[var--( --secondary-background)] text-white">
-      {/* Logo */}
-      <img src="/yt-logo.png" alt="YT-Logo" className="w-16 h-auto" />
-
-      {/* Search Bar */}
-      <div className="flex flex-1 justify-center text-white">
-        <input 
-          className="w-2/3 p-2 rounded-lg text-black outline-none border border-gray-400"
-          placeholder="Search YouTube..."
-        />
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center px-4 py-2'>
+      <div className='flex items-center gap-x-4'>
+        <button className='p-[5px] rounded-full hover:bg-[var(--secondary-background)] transition'>
+          <AlignJustify  className='cursor-pointer'/>
+        </button>
+        <Link href="/pages.tsx">
+          <img src='/yt-logo.png' alt='YouTube Logo' className='w-24 h-auto' />
+        </Link>
+      </div>  
+      <div className='flex flex-1 justify-center'> {/*Search Bar*/}
+        <input className=' max-w-lg w-full p-2 rounded-full text-white outline-none border border-b-gray-800 cursor-text' 
+        placeholder='Search YouTube'  />
       </div>
-
-      {/* Text Section */}
-      <div>
-        <h1 className="text-lg font-semibold">Hello World</h1>
-      </div>
-    </div> 
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,5 +8,5 @@ export default function Home() {
         <Header />
       </div>
     </div>
-  )
+  );
 }
