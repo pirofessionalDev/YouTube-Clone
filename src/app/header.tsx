45d96f715a8443375a974207b80fd402d6073333
pirofestionalDev <r4roggit1@gmail.com>
2025-03-06 03:27:23 +0530

    added mic button

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AlignJustify,Search,Bell } from 'lucide-react';
+import { AlignJustify,Search,Bell,Mic } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Header() {
@@ -13,7 +13,7 @@ export default function Header() {
           <img src='/yt-logo.png' alt='YouTube Logo' className='w-24 h-auto' />
         </Link>
       </div>  {/*Search Bar*/}
-      <div className="flex flex-1 justify-center">
+      <div className="flex flex-1 justify-center items-center gap-x-4">
         <div className="relative max-w-lg w-full">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />{/*ChatGPT generated code hai yeh*/}
           <input
@@ -21,6 +21,8 @@ export default function Header() {
             placeholder="Search YouTube"
           />
         </div>
+            <button className='p-[5px] rounded-full hover:bg-[var(--button-hover-background)] transistion'>
+              <Mic className='cursor-pointer' /></button>
       </div>
       <div className='flex justify-between items-center gap-x-4'>
         <button className='w-[100px] h-[30px] rounded-l-full rounded-r-full flex items-center justify-center bg-[var(--secondary-background)] hover:bg-[var(--button-hover-background)]'>
